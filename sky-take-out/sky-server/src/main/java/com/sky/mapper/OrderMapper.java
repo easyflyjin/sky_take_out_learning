8f2fe42e75c0d52d48f8c7eec5989250af896b23
easyflyjin <yjin43@ur.rochester.edu>
2024-09-24 17:20:59 +0800

    Changes

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,8 +1,12 @@
 package com.sky.mapper;
 
+import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface OrderMapper {
 
+    @Select("select * from sky_take_out.orders where id = #{id}")
+    Orders findOrderById(Long id);
 }
--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package com.sky.service.impl;
 
+import com.sky.entity.Orders;
+import com.sky.exception.OrderBusinessException;
 import com.sky.mapper.OrderMapper;
 import com.sky.properties.WeChatProperties;
 import com.sky.service.OrderService;
@@ -15,11 +17,15 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public void cancelOrder(Long id) {
         //查询ID是否存在
-        HttpClientUtil httpClientUtil = new HttpClientUtil();
+        Orders orders = orderMapper.findOrderById(id);
+        if (orders == null) {
+            throw new OrderBusinessException("用户不存在，无法删除");
+        }
+        //'订单状态 1待付款 2待接单 3已接单 4派送中 5已完成 6已取消 7退款'
+        //查询订单状态，2-4退款，1不退款，5，6不可取消，7已退款，无需再退款
+        //如果状态是6则执行退款操作
 
 
-        //根据
 
-        //退款
     }
 }
